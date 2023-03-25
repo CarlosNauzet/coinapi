7f1c6558ae5e33e50b8ecaf03260cd2b75bd6362
Nauzet Hernández <carlos.hernandez@motork.io>
2023-03-25 12:06:50 +0100

    feat: consulta de tipo de cambio

---
+++ exchange.py
@@ -0,0 +1,36 @@
+import requests
+
+apikey = '54CC9DB5-A9E5-4DEF-AFCA-BFAEE77C7995'
+api_url = 'http://rest-sandbox.coinapi.io'
+# api_url = 'http://rest.coinapi.io'
+endpoint = '/v1/exchangerate'
+
+headers = {
+    'X-CoinAPI-Key': apikey
+}
+
+
+# Pedir moneda origen: BTC
+# Pedir moneda destino: EUR
+# Preguntat el cambio a la API
+# Recoger el dato
+# Mostrar un mensaje "Un XXX vale lo mismo que NNN YYY"
+#                    "UN BTC vale lo mismo que 23000 EUR"
+# Preguntar: ¿Quieres consultar de nuevo? (S/N)
+
+
+seguir = 's'
+
+while seguir.lower() == 's':   
+    moneda_origen = input('¿Qué moneda quieres cambiar? ')
+    moneda_destino = input('¿Qué moneda deseas obtener? ')
+    url = f'{api_url}{endpoint}/{moneda_origen}/{moneda_destino}'
+    response = requests.get(url, headers=headers)
+    exchange = response.json()
+    rate = exchange.get("rate")
+
+    print(f'Un {moneda_origen} vale lo mismo que {rate:,.2f} {moneda_destino}')
+
+    seguir = ''
+    while seguir.lower() not in ('s', 'n'):
+        seguir = input('¿Quieres consultar de nuevo (S/N) ')
